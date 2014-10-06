49000d4413fd7ca4663d26ee346145c09e7069f5
thiagopnts <thiagopnts@gmail.com>
2014-10-06 14:20:26 -0300

    expose selection sort and insertion sort

--- src/sorting/mod.rs
+++ src/sorting/mod.rs
@@ -1,6 +1,10 @@
 
 pub use self::mergesort::mergesort;
 pub use self::quicksort::quicksort;
+pub use self::selection_sort::selection_sort;
+pub use self::insertion_sort::insertion_sort;
 
 mod mergesort;
 mod quicksort;
+mod selection_sort;
+mod insertion_sort;

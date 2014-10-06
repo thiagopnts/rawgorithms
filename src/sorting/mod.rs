681c2152baf9879ddb5945ca1f0150101ecd50ae
thiagopnts <thiagopnts@gmail.com>
2014-10-06 13:05:08 -0300

    add quicksort

--- src/sorting/mod.rs
+++ src/sorting/mod.rs
@@ -1,6 +1,8 @@
 
 pub use self::mergesort::mergesort;
+pub use self::quicksort::quicksort;
 pub use self::knuth_shuffle::knuth_shuffle;
 
 mod mergesort;
+mod quicksort;
 mod knuth_shuffle;
---
+++ src/sorting/quicksort.rs
@@ -0,0 +1,42 @@
+
+pub fn quicksort<T: Clone + PartialOrd>(a: &mut Vec<T>) {
+    // shuffle array
+    let len = a.len() - 1;
+    sort(a, 0, len as int);
+}
+
+fn sort<T: Clone + PartialOrd>(a: &mut Vec<T>, lo: int, hi: int) {
+    if hi <= lo { return; }
+    let j = partition(a, lo, hi);
+    sort(a, lo, j - 1);
+    sort(a, j + 1, hi);
+}
+
+fn partition<T: Clone + PartialOrd>(a: &mut Vec<T>, lo: int, hi: int) -> int {
+    let mut i = lo as uint;
+    let mut j = hi as uint;
+    loop {
+        while a.get(i) < a.get(lo as uint) {
+            i += 1;
+            if i == hi as uint { break; }
+        }
+
+        while a.get(lo as uint) < a.get(j) {
+            j -= 1;
+            if j == lo as uint { break; }
+        }
+
+        if i >= j { break; }
+        exch(a, i, j);
+    }
+
+    exch(a, lo as uint, j);
+    j as int
+}
+
+fn exch<T: Clone + PartialOrd>(a: &mut Vec<T>, i: uint, j: uint) {
+    let from = a.get(i).clone();
+    let to = a.get(j).clone();
+    *a.get_mut(i) = to;
+    *a.get_mut(j) = from;
+}

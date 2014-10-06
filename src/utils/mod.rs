4b6b3b36863ae56199d49da908d2d3076c0857b9
thiagopnts <thiagopnts@gmail.com>
2014-10-06 14:18:34 -0300

    move exch function to utils module

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -13,6 +13,13 @@ pub fn knuth_shuffle(n: uint) -> Vec<uint> {
     vec
 }
 
+pub fn exch<T: Clone + PartialOrd>(a: &mut Vec<T>, i: uint, j: uint) {
+    let from = a.get(i).clone();
+    let to = a.get(j).clone();
+    *a.get_mut(i) = to;
+    *a.get_mut(j) = from;
+}
+
 pub fn is_sorted<T: PartialOrd>(a: &Vec<T>) -> bool {
     for i in range(1, a.len()) {
         if a.get(i - 1) > a.get(i) { return false; }

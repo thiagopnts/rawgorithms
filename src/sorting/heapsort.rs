1f03f7f892a66bb1c1cd2a6b1bb4cd69d9551799
thiagopnts <thiagopnts@gmail.com>
2014-10-18 01:08:27 -0300

    add heapsort

---
+++ src/sorting/heapsort.rs
@@ -0,0 +1,32 @@
+use utils::exch;
+// heapsort works for one-based indexed arrays so it will ignore the first element of the
+// given array
+pub fn heapsort<T>(array: &mut Vec<T>) where T: Clone + PartialOrd {
+    let mut len = array.len() - 1;
+    let mut k = len / 2;
+    while k >= 1 {
+        sink(array, k, len);
+        k -= 1
+    }
+
+    while len > 1 {
+        exch(array, 1, len);
+        len -= 1;
+        sink(array, 1, len);
+    }
+}
+
+fn sink<T>(array: &mut Vec<T>, k: uint, n: uint) where T: Clone + PartialOrd {
+    let mut i = k;
+    while (2 * i) <= n {
+        let mut j = 2 * i;
+        if j < n && array.get(j) < array.get(j + 1) {
+            j += 1
+        }
+        if !(array.get(i) < array.get(j)) {
+            break;
+        }
+        exch(array, i, j);
+        i = j
+    }
+}
--- src/sorting/mod.rs
+++ src/sorting/mod.rs
@@ -4,9 +4,11 @@ pub use self::quicksort::quicksort;
 pub use self::selection_sort::selection_sort;
 pub use self::insertion_sort::insertion_sort;
 pub use self::shellsort::shellsort;
+pub use self::heapsort::heapsort;
 
 mod mergesort;
 mod quicksort;
 mod selection_sort;
 mod insertion_sort;
 mod shellsort;
+mod heapsort;

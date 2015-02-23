acb6f15d70a9d65e94b8525ee834690d0bec7c9a
thiagopnts <thiagopnts@gmail.com>
2015-02-22 21:46:17 -0300

    add tests for priority queue

--- src/data_structures/priority_queue.rs
+++ src/data_structures/priority_queue.rs
@@ -52,7 +52,7 @@ impl<T> PriorityQueue<T> where T: PartialOrd + Clone {
     fn max_index(&self) -> usize {
         let mut max = 0u;
         for i in range(1, self.pq.len() - 1) {
-            if self.pq[max] < self.pq[i + 1] {
+            if self.pq[max] < self.pq[i] {
                 max = i;
             }
         }
@@ -67,5 +67,72 @@ impl<T> PriorityQueue<T> where T: PartialOrd + Clone {
             }
         }
         min
+    }
+}
+
+mod test {
+    use super::PriorityQueue;
+
+    #[test]
+    fn test_keep_min() {
+        let mut pq = PriorityQueue::new();
+        pq.insert(3);
+        pq.insert(2);
+        pq.insert(1);
+        pq.insert(4);
+        assert!(pq.min().unwrap() == 1);
+    }
+
+    #[test]
+    fn test_keep_max() {
+        let mut pq = PriorityQueue::new();
+        pq.insert(2);
+        pq.insert(4);
+        pq.insert(1);
+        pq.insert(3);
+        assert!(pq.max().unwrap() == 4);
+    }
+
+    #[test]
+    fn test_is_empty() {
+        let mut pq = PriorityQueue::new();
+        assert!(pq.is_empty());
+        pq.insert(1);
+        assert!(!pq.is_empty());
+    }
+
+    #[test]
+    fn test_len() {
+        let mut pq = PriorityQueue::new();
+        assert!(pq.len() == 0);
+        pq.insert(2);
+        pq.insert(4);
+        pq.insert(1);
+        assert!(pq.len() == 3);
+    }
+
+    #[test]
+    fn test_delete_min() {
+        let mut pq = PriorityQueue::new();
+        pq.insert(3);
+        pq.insert(2);
+        pq.insert(1);
+        pq.insert(4);
+        assert!(pq.len() == 4);
+        assert!(pq.delete_min().unwrap() == 1);
+        assert!(pq.len() == 3);
+    }
+
+    #[test]
+    fn test_delete_max() {
+        let mut pq = PriorityQueue::new();
+        pq.insert(2);
+        pq.insert(10);
+        pq.insert(1);
+        pq.insert(6);
+        pq.insert(3);
+        assert!(pq.len() == 5);
+        assert!(pq.delete_max().unwrap() == 10);
+        assert!(pq.len() == 4);
     }
 }
